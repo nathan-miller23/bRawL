22edab7690d087fe89c5441cdbc3c97490f823a6
Oleksii Volkovskyi <volkovskyi@berkeley.edu>
2020-11-01 14:59:37 -0800

    integrate model and ppo train

--- train.py
+++ train.py
@@ -1,25 +1,61 @@
-# Begin with some imports
+# Library Imports
+import os
 import torch
 import ray
 import gym
 from IPython import display
 import matplotlib
 import matplotlib.pyplot as plt
+from ray.rllib.models import ModelCatalog
+from ray.rllib.models.torch.torch_modelv2 import TorchModelV2
+from ray.tune.registry import register_env
+from ray.rllib.agents.ppo.ppo import PPOTrainer
+
+# Local Imports
 from environments import BreakoutRllib
+from models import *
 
 # Create an experiment object
 from sacred import Experiment
 ex = Experiment("My Experiment", interactive=True)
 
+# Necessary work-around to make sacred pickling compatible with rllib
+from sacred import SETTINGS
+SETTINGS.CONFIG.READ_ONLY_CONFIG = False
+
+# Whether we're running on the server or not
+LOCAL_TESTING = os.getenv('RUN_ENV', 'local') == 'local'
+
+# Sacred Slack Observer
+from sacred.observers import SlackObserver
+if os.path.exists('slack.json') and not LOCAL_TESTING:
+    slack_obs = SlackObserver.from_config('slack.json')
+    ex.observers.append(slack_obs)
+    # Necessary for capturing stdout in multiprocessing setting
+    SETTINGS.CAPTURE_MODE = 'sys'
+
+    
+def _env_creator(env_config):
+    return gym.make(env_config['name'])
+
+def get_trainer_from_params(params):
+    return PPOTrainer(env=params['env_name'], config=params['rllib_params'])
+
 @ex.config
-def dqn_config():
+def config():
     # Define any parameters and its defaults here
     
     use_gym_env = True
     env_name = "Breakout-v0"
     use_gpu = False
-    model = "DQN"
+    model = "PPO"
     framework = "torch"
+    
+    checkpoint_path = ""
+    num_training_iters = 10
+    num_workers = 1
+    lr = 0.001
+    train = True
     
     # Environment. Gym or your own.
     # Type of algorithm
@@ -29,10 +65,29 @@ def dqn_config():
     params = {
         "use_gym_env" : use_gym_env,
         "env_name" : env_name,
+        
         "use_gpu" : use_gpu,
         "model" : model,
         "framework": framework,
+        'num_training_iters': num_training_iters,
+        
+        "checkpoint_path": checkpoint_path,
+        "train": train,
+        
+        'rllib_params': {
+            "env_config": {
+                "name": 'Breakout-v0',
+            },
+            "model": {
+                "custom_model": "my_model",
+            },
+            "vf_share_layers": True,
+            "lr": lr,
+            "num_workers": num_workers,  # parallelism
+            "framework": "torch"
+        },
     }
+    
 @ex.main
 def main(params):
     for (key, value) in params.items():
@@ -40,7 +95,7 @@ def main(params):
         
     if not params["use_gym_env"]:
         print(1)
-        from ray.tune.registry import register_env
+        
         print(2)
         global env_class
         env_class = params["env_name"]+"()"
@@ -61,6 +116,34 @@ def main(params):
         print(6)
         for i in range(100):
             print(trainer.train()['episode_reward_mean'])
+    if params["model"] == "PPO":
+        ray.init()
+        ModelCatalog.register_custom_model("my_model", TorchCustomModel)
+        trainer = get_trainer_from_params(params)
+
+        if params["train"]:
+            for i in range(params['num_training_iters']):
+                print("starting training iteration {}".format(i))
+                trainer.train()
+                if i == params['num_training_iters'] - 1:
+                    checkpoint_path = trainer.save()
+                    print(checkpoint_path)
+#     else:
+#     trainer.restore(checkpoint_path)
+#     trainer.restore("/root/ray_results/PPO_my_env_2020-11-01_19-04-39dkp16k9z/checkpoint_10/checkpoint-10")
+#     env = _env_creator(params['rllib_params']['env_config'])
+#     observation = env.reset()
+#     for i in range(100):
+#         plt.imshow(env.render(mode='rgb_array'))
+#         display.display(plt.gcf())
+#         display.clear_output(wait=True)
+#         action = trainer.compute_action(observation)
+#         observation, reward, done, info = env.step(action)
+#         print("Reward: {}".format(reward))
+#         if done: 
+#             print("Episode finished after {} timesteps".format(i+1))
+#             break
+#     env.close()
 
 if __name__ == '__main__':
     ex.run_commandline()
