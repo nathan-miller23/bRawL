d592ee6eb6048f38a12b4f1730d6bd26d06a0ae8
Kevin <chevin_ken@berkeley.edu>
2020-11-03 12:19:18 -0800

    PPo training script with custom model

--- ppo/ppo_train.py
+++ ppo/ppo_train.py
@@ -29,7 +29,7 @@ if os.path.exists('slack.json') and not LOCAL_TESTING:
     SETTINGS.CAPTURE_MODE = 'sys'
 
 def _env_creator(env_config):
-    return gym.make(env_config['name'])
+    return gym.make("BreakoutNoFrameskip-v4")
 
 def get_trainer_from_params(params):
     return PPOTrainer(env=params['env'], config=params['rllib_params'])
@@ -37,59 +37,193 @@ def get_trainer_from_params(params):
 #Smash env creator function
 
 @ex.config
-def config():
-    checkpoint_path = ""
-    num_training_iters = 10
-    num_workers = 1
-    lr = 0.001
-    train = True
-    params = {
-        "checkpoint_path": checkpoint_path,
-        "train": train,
-        "env": "my_env",
-        'rllib_params': {
-            "env_config": {
-                "name": 'Breakout-v0',
-            },
-            "model": {
-                "custom_model": "my_model",
-            },
-            "vf_share_layers": True,
-            "lr": lr,
-            "num_workers": num_workers,  # parallelism
-            "framework": "torch"
-        },
-        'num_training_iters': num_training_iters
+def my_config():
+    ### Model params ###
+
+    # whether to use recurrence in ppo model
+    use_lstm = False
+
+    # Base model params
+    NUM_HIDDEN_LAYERS = 3
+    SIZE_HIDDEN_LAYERS = 64
+    NUM_FILTERS = 25
+    NUM_CONV_LAYERS = 3
+
+    # LSTM memory cell size (only used if use_lstm=True)
+    CELL_SIZE = 256
+
+    ### Training Params ###
+
+    num_workers = 20 if not LOCAL_TESTING else 2
+
+    # list of all random seeds to use for experiments, used to reproduce results
+    seeds = [0]
+
+    # Placeholder for random for current trial
+    seed = None
+
+    # Number of gpus the central driver should use
+    num_gpus = 0 if LOCAL_TESTING else 2
+
+    # How many environment timesteps will be simulated (across all environments)
+    # for one set of gradient updates. Is divided equally across environments
+    # train_batch_size = 40000 if not LOCAL_TESTING else 800
+    train_batch_size = 100000 if not LOCAL_TESTING else 800
+
+    # size of minibatches we divide up each batch into before
+    # performing gradient steps
+    # sgd_minibatch_size = 10000 if not LOCAL_TESTING else 800
+    sgd_minibatch_size = 25000 if not LOCAL_TESTING else 800
+
+    # Rollout length
+    rollout_fragment_length = 400
+    
+    # Whether all PPO agents should share the same policy network
+    shared_policy = True
+
+    # Number of training iterations to run
+    num_training_iters = 400 if not LOCAL_TESTING else 2
+
+    # Stepsize of SGD.
+    lr = 5e-3
+
+    # Learning rate schedule.
+    lr_schedule = None
+
+    # If specified, clip the global norm of gradients by this amount
+    grad_clip = 0.1
+
+    # Discount factor
+    gamma = 0.99
+
+    # Exponential decay factor for GAE (how much weight to put on monte carlo samples)
+    # Reference: https://arxiv.org/pdf/1506.02438.pdf
+    lmbda = 0.98
+
+    # Whether the value function shares layers with the policy model
+    vf_share_layers = True
+
+    # How much the loss of the value network is weighted in overall loss
+    vf_loss_coeff = 1e-4
+
+    # Entropy bonus coefficient, will anneal linearly from _start to _end over _horizon steps
+    entropy_coeff_start = 0.02
+    entropy_coeff_end = 0.00005
+    entropy_coeff_horizon = 3e5
+
+    # Initial coefficient for KL divergence.
+    kl_coeff = 0.2
+
+    # PPO clipping factor
+    clip_param = 0.05
+
+    # Number of SGD iterations in each outer loop (i.e., number of epochs to
+    # execute per train batch).
+    num_sgd_iter = 8 if not LOCAL_TESTING else 1
+
+    # How many trainind iterations (calls to trainer.train()) to run before saving model checkpoint
+    save_freq = 250
+
+    # How many training iterations to run between each evaluation
+    evaluation_interval = 50 if not LOCAL_TESTING else 1
+
+    # How many timesteps should be in an evaluation episode
+    evaluation_ep_length = 400
+
+    # Number of games to simulation each evaluation
+    evaluation_num_games = 2
+
+    # Whether to display rollouts in evaluation
+    evaluation_display = True
+
+    # Where to store replay txt files
+    evaluation_replay_store_dir = None
+
+    # Where to log the ray dashboard stats
+    temp_dir = os.path.join(os.path.abspath(os.sep), "tmp", "ray_tmp") if not LOCAL_TESTING else None
+
+    # Where to store model checkpoints and training stats
+    results_dir = os.path.join(os.path.abspath('.'), 'results_client_temp')
+
+    # Whether tensorflow should execute eagerly or not
+    eager = False
+
+
+    ### BC Params ###
+    # path to pickled policy model for behavior cloning
+    bc_model_dir = os.path.join(BC_SAVE_DIR, "default")
+
+    # Whether bc agents should return action logit argmax or sample
+    bc_stochastic = True
+
+
+    # Name of directory to store training results in (stored in ~/ray_results/<experiment_name>)
+    experiment_name = "{0}_{1}".format("PPO_fp_", params_str)
+
+
+    # Whether dense reward should come from potential function or not
+    use_phi = True
+
+    # Max episode length
+    horizon = 400
+
+    # The number of MDP in the env.mdp_lst
+    num_mdp = 1
+    # num_mdp = np.inf  # for infinite mdp
+
+    # Constant by which shaped rewards are multiplied by when calculating total reward
+    reward_shaping_factor = 1.0
+
+    # Linearly anneal the reward shaping factor such that it reaches zero after this number of timesteps
+    reward_shaping_horizon = 1e6
+
+    # To be passed into rl-lib model/custom_options config
+    model_params = {
+        "use_lstm" : use_lstm,
+        "NUM_HIDDEN_LAYERS" : NUM_HIDDEN_LAYERS,
+        "SIZE_HIDDEN_LAYERS" : SIZE_HIDDEN_LAYERS,
+        "NUM_FILTERS" : NUM_FILTERS,
+        "NUM_CONV_LAYERS" : NUM_CONV_LAYERS,
+        "CELL_SIZE" : CELL_SIZE
     }
+    params= {
+        "num_training_iters": 500,
+        "rllib_params":
+        {"preprocessor_pref":"deepmind",
+        "num_workers" : num_workers,
+        "train_batch_size" : train_batch_size,
+        "sgd_minibatch_size" : sgd_minibatch_size,
+        "rollout_fragment_length" : rollout_fragment_length,
+        "num_sgd_iter" : num_sgd_iter,
+        "lr" : lr,
+        "lr_schedule" : lr_schedule,
+        "grad_clip" : grad_clip,
+        "gamma" : gamma,
+        "lambda" : lmbda,
+        "vf_share_layers" : vf_share_layers,
+        "vf_loss_coeff" : vf_loss_coeff,
+        "kl_coeff" : kl_coeff,
+        "clip_param" : clip_param,
+        "num_gpus" : num_gpus,
+        "seed" : seed,
+        "evaluation_interval" : evaluation_interval,
+        "entropy_coeff_schedule" : [(0, entropy_coeff_start), (entropy_coeff_horizon, entropy_coeff_end)],
+        "eager" : eager,
+        "model" : {"custom_options": model_params, "custom_model": "my_model"}}
+    }
+}
 
 @ex.automain
 def main(params):
     ray.init()
-    ModelCatalog.register_custom_model("my_model", TorchCustomModel)
+    ModelCatalog.register_custom_model("RllibPPOModel", TorchCustomModel)
     register_env("my_env", _env_creator)
 
     trainer = get_trainer_from_params(params)
     
-    # if params["train"]:
-    #     for i in range(params['num_training_iters']):
-    #         print("starting training iteration {}".format(i))
-    #         trainer.train()
-    #         if i == params['num_training_iters'] - 1:
-    #             checkpoint_path = trainer.save()
-    #             print(checkpoint_path)
-    # else:
-    # trainer.restore(checkpoint_path)
-    trainer.restore("/root/ray_results/PPO_my_env_2020-11-01_19-04-39dkp16k9z/checkpoint_10/checkpoint-10")
-    env = trainer.workers.local_worker().env
-    observation = env.reset()
-    for i in range(100):
-        plt.imshow(env.render(mode='rgb_array'))
-        display.display(plt.gcf())
-        display.clear_output(wait=True)
-        action = trainer.compute_action(observation)
-        observation, reward, done, info = env.step(action)
-        print("Reward: {}".format(reward))
-        if done: 
-            print("Episode finished after {} timesteps".format(i+1))
-            break
-    env.close()
+    for i in range(params['num_training_iters']):
+        print("starting training iteration {}".format(i))
+        trainer.train()
+        if (i % params['num_training_iters'] == 50) or (i == params['num_training_iters'] - 1):
+            checkpoint_path = trainer.save()
+            print(checkpoint_path)
