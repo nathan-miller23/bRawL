824dae9a88e352431682c4ca5f2e1917b34f05ce
Jim Wang <wangjim19@berkeley.edu>
2020-11-21 18:42:58 -0800

    modified brawl dqn script to work with rllib_compat libmelee branch

--- brawl-training/brawl_dqn.py
+++ brawl-training/brawl_dqn.py
@@ -7,14 +7,14 @@ import os, sys
 
 #Following lines are for assigning parent directory dynamically.
 
-dir_path = os.path.dirname(os.path.realpath('libmelee/SSBMEnv.py'))
+dir_path = os.path.dirname(os.path.realpath('libmelee/melee/SSBMEnv.py'))
 
 parent_dir_path = os.path.abspath(os.path.join(dir_path, os.pardir))
 
 sys.path.insert(0, parent_dir_path)
 
 
-from SSBMEnv import SSBMEnv
+from melee.SSBMEnv import SSBMEnv
 from ray.tune.registry import register_env
 
 ray.init()
@@ -31,7 +31,7 @@ def env_creator(env_config):
 register_env("SSBM", env_creator)
 trainer = dqn.DQNTrainer(env="SSBM", config = {
     "framework": "torch",
-    "env_config": {'dolphin_exe_path': '/Users/jimwang/Desktop/FM-v5.9-Slippi-r18-Mac/Dolphin.app/Contents/MacOS','ssbm_iso_path': '/Users/jimwang/Desktop/launchpad/SSMB.iso'}
+    "env_config": {'dolphin_exe_path': '/Users/jimwang/Desktop/launchpad/bRawL/mocker/dolphin-emu.app/Contents/MacOS','ssbm_iso_path': '/Users/jimwang/Desktop/launchpad/SSMB.iso'}
 })
 
 
