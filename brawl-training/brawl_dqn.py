1dc6b78b99fb6e2ab555ad06085ecb901334bc50
Jim Wang <wangjim19@berkeley.edu>
2020-11-29 14:35:55 -0800

    newest checkpoint stored in brawl dqn

--- brawl-training/brawl_dqn.py
+++ brawl-training/brawl_dqn.py
@@ -22,21 +22,18 @@ ray.init()
 def env_creator(env_config):
     return SSBMEnv(env_config['dolphin_exe_path'], env_config['ssbm_iso_path'])
 
-# DEBUGGING INFO
-#   had to rename Contents/MacOS/Dolphin to dolphin-emu
-#   had to mkdir Pipes in Contents/Resources/User
-
 
 
 register_env("SSBM", env_creator)
 trainer = dqn.DQNTrainer(env="SSBM", config = {
     "framework": "torch",
     "env_config": {'dolphin_exe_path': '/Users/jimwang/Desktop/launchpad/bRawL/mocker/dolphin-emu.app/Contents/MacOS','ssbm_iso_path': '/Users/jimwang/Desktop/launchpad/SSMB.iso'},
-    "exploration_config": {"epsilon_timesteps": 100000000}
+    "exploration_config": {"epsilon_timesteps": 5000000},
+    "hiddens": [256, 256, 256]
 })
 
-
-for i in range(100):
+trainer.restore('/Users/jimwang/ray_results/DQN_SSBM_2020-11-28_23-42-54ed1wjkkl/checkpoint_1092/checkpoint-1092')
+for i in range(5000):
     info = trainer.train()
     print("episode reward mean:", info['episode_reward_mean'], "num steps trained:", info['info']["num_steps_trained"])
     if (i % 10 == 0):
